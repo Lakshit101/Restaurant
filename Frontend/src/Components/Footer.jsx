f238888e9e89bbcc2a26a14df76be1850776c24f
Lakshit <Lakshitpaliwal9@gmail.com>
2025-04-02 12:00:20 +0530

    Initial commit

---
+++ Frontend/src/Components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">LAKSHIT</div>
+          <div className="right">
+            <p>Shubhash Chourah, Udaipur</p>
+            <p>Open: 02:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By LAKSHIT PALIWAL</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By LAKSHIT.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
