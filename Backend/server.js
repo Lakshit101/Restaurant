620760bb06edfe54d82b20aac2cc65b6c193942f
Lakshit101 <117973321+Lakshit101@users.noreply.github.com>
2025-04-06 01:09:41 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,7 @@
-// import app from './app.js';
-
-// app.listen(process.env.PORT,()=>{
-//     console.log(`Server is running on port ${process.env.PORT}`);
-// })
-
 import app from './app.js';
-import dotenv from 'dotenv';
 
-dotenv.config({ path: './config/config.env' });
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
 
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(Server is running on port ${PORT});
-});
+ 
