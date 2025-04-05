da4e936b8922d9e78cc8ea11ca57a1a5b561d034
Lakshit101 <117973321+Lakshit101@users.noreply.github.com>
2025-04-06 01:40:43 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,7 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
-app.use('https://restaurant-frontend-6gql.onrender.com/reservation',reservationRouter);
+app.use('api/v1/reservation',reservationRouter);
 // /api/v1/reservation
 dbConnection();
 app.use(errorMiddleware);
