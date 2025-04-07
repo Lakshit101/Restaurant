5a7a5b4067b4e811d81a6a38c453b8d0e36ee7ed
Lakshit101 <117973321+Lakshit101@users.noreply.github.com>
2025-04-07 18:36:03 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -29,7 +29,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
-app.use('api/v1/reservation',reservationRouter);
+app.use('/api/v1/reservation',reservationRouter);
 // /api/v1/reservation
 dbConnection();
 app.use(errorMiddleware);
